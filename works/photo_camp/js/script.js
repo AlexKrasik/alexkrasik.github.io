76710afbdbda6389a4f1b4488bfba7fabf36faeb
Aleksandr Krasikov <alexkrasik@gmail.com>
2021-08-23 18:16:30 +0300

    wow js added

--- works/photo_camp/js/script.js
+++ works/photo_camp/js/script.js
@@ -1,27 +1,28 @@
-var tag = document.createElement('script');
+new WOW().init();
+// var tag = document.createElement('script');
 
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// tag.src = "https://www.youtube.com/iframe_api";
+// var firstScriptTag = document.getElementsByTagName('script')[0];
+// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-var player;
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('rickroll', {
-        height: '390',
-        width: '640',
-        videoId: 'dQw4w9WgXcQ',
-        playerVars: {
-            'playsinline': 1
-        },
-        events: {
-            // 'onReady': onPlayerReady,
-            // 'onStateChange': onPlayerStateChange
-        }
-    });
-}
+// var player;
+// function onYouTubeIframeAPIReady() {
+//     player = new YT.Player('rickroll', {
+//         height: '390',
+//         width: '640',
+//         videoId: 'dQw4w9WgXcQ',
+//         playerVars: {
+//             'playsinline': 1
+//         },
+//         events: {
+//             // 'onReady': onPlayerReady,
+//             // 'onStateChange': onPlayerStateChange
+//         }
+//     });
+// }
+
 
 $(function () {
-
     $('.popup-bg').click(function () {
         $('.popup-bg, .popup-window').hide();
         player.stopVideo();
