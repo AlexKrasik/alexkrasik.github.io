b1048e2cabffabcd91c72605bd25c5ea57344389
Aleksandr Krasikov <alexkrasik@gmail.com>
2021-10-01 13:03:45 +0300

    animations added

--- pateeva/script.js
+++ pateeva/script.js
@@ -1,43 +1,50 @@
 $(function () {
-    $("#show .slider img").toArray().forEach(element => {
-      $(element).attr('src', $(element).attr('data-src'));
+  new WOW().init();
+  //lazy load
+  $("#show .slider img")
+    .toArray()
+    .forEach((element) => {
+      $(element).attr("src", $(element).attr("data-src"));
     });
-    $("#show .slider").slick({
-      infinite: true,
-      slidesToShow: 2,
-      slidesToScroll: 2,
-      dots: true,
-      appendDots: '#show .slider__dots',
-      prevArrow: '#show .prev',
-      nextArrow: '#show .next',
-      responsive: [
+    //sliders
+  $("#show .slider").slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    dots: true,
+    appendDots: "#show .slider__dots",
+    prevArrow: "#show .prev",
+    nextArrow: "#show .next",
+    responsive: [
       {
-          breakpoint: 1160,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-          },
-        },{
-          breakpoint: 767,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            arrows: false,
-          },
+        breakpoint: 1160,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
         },
-      ],
-    });
-    $("#review .slider").slick({
-      arrows: false,
-      dots: true,
-      appendDots: '#review .slider__dots',
-      responsive: [
-        {
-            breakpoint: 767,
-            settings: {
-              adaptiveHeight: true,
-            },
-          },
-        ],
-    });
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+        },
+      },
+    ],
   });
+  $("#review .slider").slick({
+    arrows: false,
+    dots: true,
+    appendDots: "#review .slider__dots",
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          adaptiveHeight: true,
+        },
+      },
+    ],
+  });
+});
