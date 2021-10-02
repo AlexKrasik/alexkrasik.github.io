412ba3db67702832f1697fe3a727b346252bd1bb
Aleksandr Krasikov <alexkrasik@gmail.com>
2021-10-02 13:49:07 +0300

    add webp images

---
+++ pateeva/gulpfile.js
@@ -0,0 +1,8 @@
+const gulp = require('gulp');
+const webp = require('gulp-webp');
+ 
+gulp.task('default', () =>
+    gulp.src('img/*.{jpg,png}')
+        .pipe(webp({quality: 80}))
+        .pipe(gulp.dest('img'))
+);
